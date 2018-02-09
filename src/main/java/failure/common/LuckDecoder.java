2bbe50cfb05c7bd6cbf2e393df28fe13ec3fb09a
zhoudaifa007 <zhoudaifa007@gmail.com>
2018-02-09 12:23:56 +0800

    自定义编解码

--- src/main/java/failure/common/LuckDecoder.java
+++ src/main/java/failure/common/LuckDecoder.java
@@ -13,7 +13,12 @@ public class LuckDecoder extends ByteToMessageDecoder {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-
+        //少于44个字节，不读取
+        if (in.readableBytes() < (4 + 4 + 36)) {
+            return;
+        }
+        //标识readerIndex的位置
+        in.markReaderIndex();
         // 获取协议的版本
         int version = in.readInt();
         // 获取消息长度
@@ -22,15 +27,16 @@ public class LuckDecoder extends ByteToMessageDecoder {
         byte[] sessionByte = new byte[36];
         in.readBytes(sessionByte);
         String sessionId = new String(sessionByte);
-
         // 组装协议头
         LuckHeader header = new LuckHeader(version, contentLength, sessionId);
-
+        if(in.readableBytes() < contentLength) {
+            in.resetReaderIndex();
+            return;
+        }
         // 读取消息内容
-        byte[] content = in.readBytes(in.readableBytes()).array();
-
+        byte[] content = new byte[contentLength];
+        in.readBytes(content);
         LuckMessage message = new LuckMessage(header, new String(content));
-
         out.add(message);
     }
 }
